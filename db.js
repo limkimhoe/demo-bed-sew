1b7cff7b31fd97855991a50390d5588ddbe476b1
Kim Hoe Lim <spsocdevops@gmail.com>
2021-05-10 11:54:19 +0800

    update db

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ dbConnect.getConnection = function() {
         //     database: "jibaboom"
         // }
         {
-            host: "us-cdbr-iron-east-03.cleardb.net",
+            host: "us-cdbr-east-03.cleardb.com",
             user: "b0f622f9010d1e",
             password: "65840dd2",
             database: "heroku_65511d8764d886a"
