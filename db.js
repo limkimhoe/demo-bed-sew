fa69df965d64394658d71125427990a20e8261c2
Kim Hoe Lim <spsocdevops@gmail.com>
2021-05-10 11:15:23 +0800

    update db info

--- db.js
+++ db.js
@@ -4,11 +4,17 @@ var dbConnect = {};
 
 dbConnect.getConnection = function() {
     var conn = mysql.createConnection(
+        // {
+        //     host: "localhost",
+        //     user: "root",
+        //     password: "",
+        //     database: "jibaboom"
+        // }
         {
-            host: "localhost",
-            user: "root",
-            password: "",
-            database: "jibaboom"
+            host: "remotemysql.com",
+            user: "mNyDF0sDUp",
+            password: "7DmGKyNdaL",
+            database: "mNyDF0sDUp"
         }
     );
     return conn;
